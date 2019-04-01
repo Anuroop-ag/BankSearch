ae617d703e222bb32e0c2f5c680c26fa47a0839c
Anuroop-ag <anuroop.gubbala.@gmail.com>
2019-04-01 19:25:04 +0530

    Add Styling

--- bank.js
+++ bank.js
@@ -38,13 +38,19 @@ function myFunction() {
 		}      
 	}
 }
+var count = 0;
 function fetcher(){
+	var loader = document.getElementById("loader");
 
 	fetch("https://vast-shore-74260.herokuapp.com/banks?city=KOLKATA")
 		.then(res => res.json() )
 		.then(posts => {			
 			posts = sortJSON(posts, 'bank_id');	
 			kolkata=posts;
+			count++;
+			if(count == 5){
+				loader.style.display = "none";
+			}
 		})
 		
 		fetch("https://vast-shore-74260.herokuapp.com/banks?city=DELHI")
@@ -53,6 +59,10 @@ function fetcher(){
 			posts = sortJSON(posts, 'bank_id');	
 			// displayTable(posts);
 			delhi=posts;
+			count++;
+			if(count == 5){
+				loader.style.display = "none";
+			}
 		})
 
 		fetch("https://vast-shore-74260.herokuapp.com/banks?city=MUMBAI")
@@ -61,6 +71,10 @@ function fetcher(){
 			posts = sortJSON(posts, 'bank_id');	
 			// displayTable(posts);
 			mumbai=posts;
+			count++;
+			if(count == 5){
+				loader.style.display = "none";
+			}
 		})
 
 		fetch("https://vast-shore-74260.herokuapp.com/banks?city=HYDERABAD")
@@ -69,6 +83,10 @@ function fetcher(){
 			posts = sortJSON(posts, 'bank_id');	
 			// displayTable(posts);
 			hyd=posts;
+			count++;
+			if(count == 5){
+				loader.style.display = "none";
+			}
 		})
 
 		fetch("https://vast-shore-74260.herokuapp.com/banks?city=CHENNAI")
@@ -76,39 +94,52 @@ function fetcher(){
 		.then(posts => {			
 			posts = sortJSON(posts, 'bank_id');	
 			chennai=posts;
+			count++;
+			if(count == 5){
+				loader.style.display = "none";
+			}
 		})
 	
 }
 
 function kolkataData(){
 
+	if(kolkata){
+		displayTable(kolkata);		
+	}
 
-			displayTable(kolkata);
 }
 
 function delhiData(){
 
-			displayTable(delhi);
+	if(delhi){
+		displayTable(delhi);		
+	}
 		
 }
 
 function mumbaiData(){
 
-			displayTable(mumbai);
+	if(mumbai){
+		displayTable(mumbai);		
+	}
 }
 
 
 function hydData(){
 
-			displayTable(hyd);
+	if(hyd){
+		displayTable(hyd);		
+	}
 
 }
 
 
 function chennaiData(){
 
-	
-			displayTable(chennai);
+	if(chennai){
+		displayTable(chennai);		
+	}
 
 }
 
@@ -116,14 +147,30 @@ function chennaiData(){
 function displayTable(data){
 	
 	if(data.length>0){
+
+	
+
+		var tableLimitter = document.createElement("div");
+		tableLimitter.setAttribute('class', 'limiter');
+		var tableContainer = document.createElement("div");
+		tableContainer.setAttribute('class', 'container-table100');
+		var wrapper = document.createElement("div");
+		wrapper.setAttribute('class','wrap-table100');
 		
-		var table = document.createElement("table");
-		table.style.width = '20%';
-		table.setAttribute('border', '1');
-		table.setAttribute('cellspacing', '0');
-		table.setAttribute('cellpadding', '0');
-		
-		// retrieve column header ('Name', 'Email', and 'Mobile')
+		var tableDiv = document.createElement("div");
+		tableDiv.setAttribute('class', 'table100 ver1 m-b-110');
+
+		var tableHeadDiv = document.createElement("div");
+		tableHeadDiv.setAttribute('class','table100-head');
+
+		var tableHead = document.createElement("table");
+
+
+		var thead = document.createElement("thead");
+
+		var theadr = document.createElement("tr");
+
+		theadr.setAttribute('class', 'row100 head');
 
 		var col = [];
 		for (var i = 0; i < data.length; i++) {
@@ -133,47 +180,58 @@ function displayTable(data){
 					}
 			}
 		}
-		
-		// CREATE TABLE HEAD .
-		var tHead = document.createElement("thead");	
-			
-		
-		// CREATE ROW FOR TABLE HEAD .
-		var hRow = document.createElement("tr");
-		
-		// ADD COLUMN HEADER TO ROW OF TABLE HEAD.
+
 		for (var i = 0; i < col.length; i++) {
-				var th = document.createElement("th");
-				th.innerHTML = col[i];
-				hRow.appendChild(th);
+			var th = document.createElement("th");
+			th.setAttribute("class",'cell100 column' + (i+1));
+			th.innerHTML = col[i];
+			theadr.appendChild(th);
 		}
-		tHead.appendChild(hRow);
-		table.appendChild(tHead);
-		
-		// CREATE TABLE BODY .
-		var tBody = document.createElement("tbody");	
+
+		thead.appendChild(theadr);
+		tableHead.appendChild(thead);
+		tableHeadDiv.appendChild(tableHead);
+
+		var tableBodyDiv = document.createElement("div");
+		tableBodyDiv.setAttribute("class",'table100-body');
+
+		var tableBody = document.createElement("table");
+
+
+		var tbody = document.createElement("tbody");
+
+			
 		
 		// ADD COLUMN HEADER TO ROW OF TABLE HEAD.
 		for (var i = 0; i < data.length; i++) {
 		
-				var bRow = document.createElement("tr"); // CREATE ROW FOR EACH RECORD .
+			var tbodyr = document.createElement("tr");
+			tbodyr.setAttribute('class','row100 body')
+			
+			
+			for (var j = 0; j < col.length; j++) {
+				var td = document.createElement("td");
+				td.innerHTML = data[i][col[j]];
+				td.setAttribute("class",'cell100 column' + (j+1));
+
 				
-				
-				for (var j = 0; j < col.length; j++) {
-					var td = document.createElement("td");
-					td.innerHTML = data[i][col[j]];
-					bRow.appendChild(td);
-				}
-				tBody.appendChild(bRow)
+				tbodyr.appendChild(td);
+			}
+			tbody.appendChild(tbodyr)
 
 		}
-		table.appendChild(tBody);	
+		tableBody.appendChild(tbody);	
+		tableBodyDiv.appendChild(tableBody);
+		tableDiv.appendChild(tableHeadDiv);
+		tableDiv.appendChild(tableBodyDiv);
 		
-		
+		wrapper.appendChild(tableDiv);
+		tableContainer.appendChild(wrapper);
+		tableLimitter.appendChild(tableContainer);
 		// FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
 		var divContainer = document.getElementById("showData");
 		divContainer.innerHTML = "";
-		divContainer.appendChild(table);
+		divContainer.appendChild(tableLimitter);
 		
 	}	
 
